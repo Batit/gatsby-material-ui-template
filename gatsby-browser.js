dc9cff551a0ed435d1f7c3319b07cf3dc81d22fb
Vladimir Galicia <thewakosmanclub@gmail.com>
2020-04-20 17:33:18 -0500

    use wrapRootElement

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-filename-extension */
 /**
  * Implement Gatsby's Browser APIs in this file.
  *
@@ -5,3 +6,13 @@
  */
 
 // You can delete this file if you're not using it
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export const wrapRootElement = ({ element }) => (
+  <div style={{ display: "flex" }}>
+    <CssBaseline />
+    {element}
+  </div>
+);
