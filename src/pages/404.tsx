75b1bd6ff3240387a97d0f9c29b5ac1cf35e462f
nely <nely.flores@sendengo.com>
2020-09-05 00:45:38 -0500

    change in image, delete query

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,36 +1,21 @@
 import React from "react";
 
-import { graphql, useStaticQuery, Link } from "gatsby";
-import Img from "gatsby-image";
+import { Link } from "gatsby";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import ImageNotFound from "../images/404.png";
 
 import Layout from "../components/Layout/Layout.component";
 import SEO from "../components/Seo/Seo.component";
 
 const NotFoundPage: React.FC = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "404.png" }) {
-        childImageSharp {
-          fixed(width: 500) {
-            base64
-            width
-            height
-            src
-            srcSet
-          }
-        }
-      }
-    }
-  `);
   return (
     <Layout>
       <SEO title="404: Not found" />
       <Grid container direction="column" justify="center" alignItems="center">
-        <Img fixed={data.placeholderImage.childImageSharp.fixed} />
+        <img src={ImageNotFound} alt="page not found" />
         <Box m={5} textAlign="center">
           <Typography variant="h3">Page not found </Typography>
           <Typography variant="h6">
